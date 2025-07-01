ac98838bb3ff6cdf2dff03b697b7430f77de90af
Sparsh Maheshwari <sparsh.m@algoritt.com>
2025-07-01 14:35:39 +0530

    Add asyncHandler to evaluateRules routes for improved error handling

--- routes/route.evaluateRules.js
+++ routes/route.evaluateRules.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const evaluateRulesController = require('../controllers/controller.evaluateRules');
+const asyncHandler = require('../utils/util.asyncHandler');
 
-router.get('/:ruleId/:transactionId', evaluateRulesController.dryRunARuleOnATransaction);
-router.post('/', evaluateRulesController.evaluateRules);
-router.post('/2', evaluateRulesController.evaluateRules2);
-router.post('/3', evaluateRulesController.evaluateRules3);
+router.get('/:ruleId/:transactionId', asyncHandler(evaluateRulesController.dryRunARuleOnATransaction));
+router.post('/', asyncHandler(evaluateRulesController.evaluateRules));
+router.post('/2', asyncHandler(evaluateRulesController.evaluateRules2));
+router.post('/2.1', asyncHandler(evaluateRulesController.evaluateRules21));
+router.post('/3', asyncHandler(evaluateRulesController.evaluateRules3));
 
 module.exports = router;
