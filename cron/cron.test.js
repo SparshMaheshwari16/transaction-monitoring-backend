76994f6924ac67324412c12574d49cb0226d391a
Sparsh Maheshwari <sparsh.m@algoritt.com>
2025-07-08 13:34:29 +0530

    update package.json to include cross-env as a devDependency

--- cron/cron.test.js
+++ cron/cron.test.js
@@ -2,7 +2,7 @@
 const cron = require('node-cron');
 const { testing } = require('../controllers/controller.cronTest');
 
-// Cron pattern: At 00:00 on the 7th day of every month
+// Cron pattern
 cron.schedule('* * * * *', () => {
     console.log('Running task', new Date().toISOString());
     testing();
