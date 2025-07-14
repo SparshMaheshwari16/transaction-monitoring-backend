500eeaeb6a28780a2d8d3a1eb7926d5291918565
Sparsh Maheshwari <sparsh.m@algoritt.com>
2025-07-14 15:40:58 +0530

    Add updateAllActiveVariables function

--- controllers/controller.behavioralVariable.js
+++ controllers/controller.behavioralVariable.js
@@ -23,3 +23,39 @@ exports.getAll = async (req, res) => {
     });
 };
 
+exports.updateAllActiveVariables = async (req, res) => {
+    const { rows: variables } = await pool.query(`
+    SELECT * FROM behavioral_variables_definitions
+    WHERE is_active = TRUE
+  `);
+    if (!variables || variables.length === 0) {
+        throw new ApiError(404, 'No rules found');
+    }
+
+    let result = [];
+    for (const variable of variables) {
+        const { variable_name, target_table, target_column, sql_template } = variable;
+
+        const wrappedQuery = `
+      UPDATE ${target_table} uts
+      SET ${target_column} = data.value,
+          last_updated = CURRENT_TIMESTAMP
+      FROM (${sql_template}) AS data
+      WHERE uts.user_id = data.user_id;
+    `;
+
+        const { rows } = await pool.query(wrappedQuery);
+        console.log(rows);
+        console.log(`Updated variable: ${variable_name}`);
+        result.push({
+            wrappedQuery, rows
+        })
+    }
+
+    return res.status(200).json({
+        success: true,
+        message: 'All active behavioral variables updated successfully.',
+        result
+    });
+};
+
