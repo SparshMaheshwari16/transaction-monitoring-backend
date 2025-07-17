89b09c40cdffffa418b0c993033665e3b6eec695
Sparsh Maheshwari <sparsh.m@algoritt.com>
2025-07-17 19:50:39 +0530

    feat: implement cache warmup function to preload user data and rules

---
+++ utils/util.cacheWarmup.js
@@ -0,0 +1,25 @@
+const userTransactionService = require('../services/service.userTransactionSummary');
+const ruleService = require('../services/service.rule');
+const userService = require('../services/service.user'); // Assuming this has getAllUser
+
+
+async function warmupCache() {
+    try {
+        console.log('Starting cache warmup...');
+
+        await ruleService.getAllRules();
+        console.log('Rules cached');
+
+        await userTransactionService.getAllUserTransactionSummary();
+        console.log('User transaction summaries cached');
+
+        await userService.getAllUser();
+        console.log('Users cached');
+
+        console.log('Cache warmup complete!');
+    } catch (err) {
+        console.error('Cache warmup failed:', err);
+    }
+}
+
+module.exports = warmupCache;
